fdca6c5e60457d991f11f8e1221034f702890fd9
Megsie Siple <mcsiple@gmail.com>
2020-09-10 13:37:37 -0700

    add tests

--- code/tests/test_bstar.R
+++ code/tests/test_bstar.R
@@ -1,15 +1,20 @@
 # Test b.star.fun
-# 
+
 source(here::here("code","1_operating_model.R"))
-library(testthat)
-Bmsy
 
-b.equilib <- sapply(F.vec, FUN=b.star.fun, K=K, A=A, r=r)
-b.equilib <- replace(b.star.vec,which(b.star.vec=="NaN"),0)
-
+# with no fishing, pop goes to K
 test_that("No fishing",{
-  Ktest = 100
+  Ktest <- 100
   expect_equal(b.star.fun(F = 0,K = Ktest,A = 0,r = 1.28),Ktest)
 })
 
-testthat::test_dir("tests")
+# test_that("FMSY check"){
+#   Fmsy <- 0.7
+#   F.vec <- seq(0, 2*Fmsy, length.out = 100)
+#   b.star.vec <- sapply(F.vec, FUN=b.star.fun, K=K, A=A, r=r)
+#   b.star.vec <- replace(b.star.vec,which(b.star.vec=="NaN"),0)
+#   which.max(b.star.vec)
+# }
+
+# To run tests:
+#testthat::test_dir(here::here("code","tests"))
