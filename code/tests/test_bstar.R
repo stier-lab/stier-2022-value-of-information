d4be85dbec05bea7239f10810b9fe5c31b5a0d72
Megsie Siple <mcsiple@gmail.com>
2020-09-16 17:51:00 -0700

    add MSY check test

--- code/tests/test_bstar.R
+++ code/tests/test_bstar.R
@@ -8,13 +8,21 @@ test_that("No fishing",{
   expect_equal(b.star.fun(F = 0,K = Ktest,A = 0,r = 1.28),Ktest)
 })
 
-# test_that("FMSY check"){
-#   Fmsy <- 0.7
-#   F.vec <- seq(0, 2*Fmsy, length.out = 100)
-#   b.star.vec <- sapply(F.vec, FUN=b.star.fun, K=K, A=A, r=r)
-#   b.star.vec <- replace(b.star.vec,which(b.star.vec=="NaN"),0)
-#   which.max(b.star.vec)
-# }
-
-# To run tests:
-#testthat::test_dir(here::here("code","tests"))
+test_that("MSY check",{
+  A  <- 0
+  Bmsy <- 70
+  MSY <- 20
+  K <- -(3*Bmsy^2 - 2*A*Bmsy)/(A - 2*Bmsy)
+  r <- MSY/(Bmsy*(1-Bmsy/K)*(Bmsy/K-A/K))
+  
+  F.vec <- seq(0, 2, length.out = 100)
+  
+  b.star.vec <- sapply(F.vec, FUN=b.star.fun, K=K, A=A, r=r)
+  b.star.vec <- replace(b.star.vec,which(b.star.vec=="NaN"),0)
+  catch <- F.vec*b.star.vec
+  plot(F.vec,catch)
+  MSY.out <- max(catch)
+  diff <- abs(MSY - MSY.out)
+  
+  expect_true(diff<0.01)
+})
