f316210e6d5c0f6e16d99a4a41672b892cc0026d
Megsie Siple <mcsiple@gmail.com>
2020-09-16 18:00:29 -0700

    clenaup

--- code/2_model_parameters.R
+++ code/2_model_parameters.R
@@ -1,23 +1,23 @@
 # parameterize model duration and economics
 
-years = 20 #time of simulation
-B.start <-100 #starting biomass
-delta<-.05 # discount rate
-process.noise = 0 #temporally uncorrelated variance (sd) in little r 
+years <- 20 #time of simulation
+B.start <- 100 #starting biomass
+delta <- 0.05 # discount rate
+process.noise <- 0 #temporally uncorrelated variance (sd) in little r 
 
 ####pricing stuff here is pulled out of the air 
-p = 20 #price per unit biomass
-c=200 # cost to achieve F
+p <- 20 #price per unit biomass
+c <- 200 # cost to achieve F
 
 #cm = 5# old cost of monitoring. higher cm, more expensive monitoriing
 
-ci<-100 #intecept of decay in cost of monitoring function
-cs<-5 #slope in cost of monitoring function
+ci <- 100 #intecept of decay in cost of monitoring function
+cs <- 5 #slope in cost of monitoring function
 
 # parameterize the model based on A,BMSY and MSY
-A = 10  #allee effect threshold 
-Bmsy<- 70 #Biomass at MSY
-MSY<- 25
+A <- 10  #allee effect threshold 
+Bmsy <- 70 #Biomass at MSY
+MSY <- 25
 
 K<-(3*Bmsy^2 - 2*A*Bmsy)/( 2*Bmsy-A) #Carrying Capacity of Focal population (derived by TE in matlab)
 r<-MSY/(Bmsy*(1-Bmsy/K)*(Bmsy/K-A/K)) #population growth rate 
